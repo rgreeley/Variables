0d76e76f3127907ad54e632e6e99d47ea5adec11
Robert Greeley <rgreeley@zappos.com>
2014-07-28 14:28:57 -0700

    Your Schedule EX13

---
+++ YourSchedule.java
@@ -0,0 +1,38 @@
+public class YourSchedule 
+{
+	public static void main( String[] args )
+	{
+		String Leader1, Leader2, Leader3, Leader4, Leader5, Leader6, Leader7, Leader8,
+		Appointment1, Appointment2, Appointment3, Appointment4, Appointment5, Appointment6,
+	    Appointment7, Appointment8;
+				
+		Leader1 = "Mike Bitwinski";
+		Leader2 = "Thomas Stein";
+		Leader3 = "Tara Munday";
+		Leader4 = "Sean Moore";
+		Leader5 = "Christy Colegan";
+		Leader6 = "Wayne Rockwell";
+		Leader7 = "Allison Shultz";
+		Leader8 = "Brandon Lewis";
+		Appointment1 = "Recognition";
+		Appointment2 = "Progression";
+		Appointment3 = "Order Verification";
+		Appointment4 = "Phones";
+		Appointment5 = "Beutification";
+		Appointment6 = "Project 100";
+		Appointment7 = "Wishz";
+		Appointment8 = "Resource Desk";
+		
+		System.out.println("+----------------------------------------------------+" );
+		System.out.println("| 1 |" + Appointment1 + "                    |" + Leader1 + "  |" );
+		System.out.println("| 2 |" + Appointment2 + "                    |" + Leader2 + "    |" );
+		System.out.println("| 3 |" + Appointment3 + "             |" + Leader3 + "     |" );
+		System.out.println("| 4 |" + Appointment4 + "                         |" + Leader4 + "      |" );
+		System.out.println("| 5 |" + Appointment5 + "                  |" + Leader5 + " |" );
+		System.out.println("| 6 |" + Appointment6 + "                    |" + Leader6 + "  |" );
+		System.out.println("| 7 |" + Appointment7 + "                          |" + Leader7 + "  |" );
+		System.out.println("| 8 |" + Appointment8 + "                  |" + Leader8 + "   |" );
+		System.out.println("+----------------------------------------------------+" );
+		
+	}
+}
