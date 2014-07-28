93f364df9bb182f0569eca953a5dc285a5049eb2
Robert Greeley <rgreeley@zappos.com>
2014-07-28 11:07:46 -0700

    UsingVariables.java

---
+++ UsingVariables.java
@@ -0,0 +1,17 @@
+public class UsingVariables 
+{
+	public static void main( String[] args )
+	{
+		String Department;
+		int RmNumber;
+		double time;
+				
+		Department = "Computer Science";
+		RmNumber = 113;
+		time = 2.71828;
+		
+		System.out.println( "This is room " + RmNumber + "." );
+		System.out.println( "e is close to " + time );
+		System.out.println( "I am learning a bit about " + Department );
+	}
+}
