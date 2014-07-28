421a4713e911d934a44b92be40557fdeaf8d7b7f
Robert Greeley <rgreeley@zappos.com>
2014-07-28 10:56:03 -0700

    More Variables and Printing EX10

---
+++ MoreVariablesAndPrinting.java
@@ -0,0 +1,30 @@
+public class MoreVariablesAndPrinting
+{
+	public static void main( String[] args )
+	{
+		String Name, Eyes, Teeth, Hair;
+		int Age, Height, Weight;
+		double Cm, Kilos;
+				
+		Name = "Zed A. Shaw";
+		Age = 35;     // not a lie
+		Height = 74;  // inches
+		Weight = 180;   // lbs
+		Eyes = "Blue";
+		Teeth = "White";
+		Hair = "Brown";
+		Cm = Height * 2.54;
+		Kilos = Weight * 0.453592;
+		
+		
+		System.out.println( "Let's talk about " + Name + "." );
+		System.out.println( "He's " + Height + " inches tall, or " + Cm + " cm." );
+		System.out.println( "He's " + Weight + " pounds heavy, or " + Kilos + " kg." );
+		System.out.println( "Actually, that's not too heavy." );
+		System.out.println( "He's got " + Eyes + " eyes and " + Hair + " hair." );
+		System.out.println( "His teeth are usually " + Teeth + " depending on the coffee." );
+		
+		//This line is tricky; try to get it exactly right.
+		System.out.println( "If I add " + Age + ", " + Height + ", and " + Weight + " I get " + (Age + Height + Weight) + "." );
+	}
+}
