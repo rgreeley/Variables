ca876b40ba63b7d6b537e36db782368b9f958ca9
Robert Greeley <rgreeley@zappos.com>
2014-07-28 13:42:45 -0700

    Still Using Variables EX12

---
+++ StillUsingVariables.java
@@ -0,0 +1,13 @@
+public class StillUsingVariables 
+{
+	public static void main( String[] args )
+	{
+		String MyName;
+		int YearOfGrad;
+				
+		MyName = "Robert Greeley";
+		YearOfGrad = 2008;
+		
+		System.out.println( "My name is " + MyName + " and I'll graduate in " + YearOfGrad + "." );
+	}
+}
